244f2ef9b672d982e8090509ec27d79c372c17fe
GilangNyan <sagilang@gmail.com>
2025-02-12 07:55:25 +0700

    feat: print report in monthly attendance reports

--- src/views/dashboard/reports/MonthlyAttendanceView.vue
+++ src/views/dashboard/reports/MonthlyAttendanceView.vue
@@ -21,8 +21,11 @@
     </div>
   </Form>
   <div class="bg-white p-4 rounded-lg flex flex-col space-y-2" v-if="isReportGenerated">
-    <div class="flex w-full justify-end items-center">
-      <ButtonRoundedWithIcon color="indigo" type="button" :label="$t('label.downloadReports')" @click="downloadReports()">
+    <div class="flex w-full justify-end items-center space-x-2">
+      <ButtonRoundedWithIcon color="indigo" type="button" :label="$t('label.print')" @click="generateReports()" :loading="downloadPdfLoading" :disabled="downloadPdfLoading">
+        <PrinterIcon class="w-5 h-5" />
+      </ButtonRoundedWithIcon>
+      <ButtonRoundedWithIcon color="indigo" type="button" :label="$t('label.downloadReports')" @click="downloadReports()" :loading="downloadXlsxLoading" :disabled="downloadXlsxLoading">
         <DocumentArrowDownIcon class="w-5 h-5" />
       </ButtonRoundedWithIcon>
     </div>
@@ -37,12 +40,14 @@ import ButtonRoundedWithIcon from '@/components/buttons/ButtonRoundedWithIcon.vu
 import SelectForm from '@/components/inputs/SelectForm.vue';
 import BreadCrumbs from '@/components/main/BreadCrumbs.vue';
 import TableCustom from '@/components/main/TableCustom.vue';
+import PrintPreviewModal from '@/components/modals/PrintPreviewModal.vue';
 import timeConstants from '@/constants/timeConstants';
 import attendanceServices from '@/services/masterData/attendanceServices';
 import gradeServices from '@/services/masterData/gradeServices';
+import { useModalStore } from '@/stores/modal';
 import type ISelectOption from '@/types/selectOption';
-import { getTotalDaysByMonth, handleErrorResponse } from '@/utils/utilities';
-import { DocumentArrowDownIcon, MagnifyingGlassIcon } from '@heroicons/vue/24/solid';
+import { formatFullDate, getTotalDaysByMonth, handleErrorResponse } from '@/utils/utilities';
+import { DocumentArrowDownIcon, MagnifyingGlassIcon, PrinterIcon } from '@heroicons/vue/24/solid';
 import type { AxiosResponse } from 'axios';
 import { Form } from 'vee-validate';
 import { onMounted, ref, type Ref } from 'vue';
@@ -50,6 +55,9 @@ import { useI18n } from 'vue-i18n';
 import * as yup from 'yup';
 
 const { t } = useI18n()
+const modal = useModalStore()
+const htmlTemplate = ref('')
+const htmlData = ref('')
 
 const yearOptions: Ref<ISelectOption[]> = ref([])
 const monthOptions: Ref<ISelectOption[]> = ref([])
@@ -102,6 +110,8 @@ const columns: Ref = ref([
 const rows: Ref = ref([])
 const isBusy: Ref<boolean> = ref(false)
 const isReportGenerated: Ref<boolean> = ref(false)
+const downloadXlsxLoading: Ref<boolean> = ref(false)
+const downloadPdfLoading: Ref<boolean> = ref(false)
 
 const getYears = (): void => {
   const d = new Date
@@ -210,6 +220,54 @@ const downloadReports = async () => {
     document.body.removeChild(link);
     URL.revokeObjectURL(url);
 
+  })
+}
+
+const generateReports = async () => {
+  downloadPdfLoading.value = true
+  const response = await fetch('/monthly-attendance.html')
+  htmlTemplate.value = await response.text()
+  await populateReports()
+  const date = new Date()
+  htmlTemplate.value = htmlTemplate.value.replace('<tbody></tbody>', `<tbody>${htmlData.value}</tbody>`)
+  htmlTemplate.value = htmlTemplate.value.replace('<td id="date"></td>', `<td id="date">${formatFullDate(date.toISOString())}</td>`)
+  htmlTemplate.value = htmlTemplate.value.replace('<td id="academic-year"></td>', `<td id="academic-year">${year.value?.label}</td>`)
+  htmlTemplate.value = htmlTemplate.value.replace('<td id="grade"></td>', `<td id="grade">${grade.value?.label}</td>`)
+  modal.openModal({ component: PrintPreviewModal, props: { content: htmlTemplate.value } })
+  downloadPdfLoading.value = false
+}
+
+const populateReports = async () => {
+  const payload = {
+    year: year.value?.value,
+    month: month.value?.value,
+    grade: grade.value?.value
+  }
+  htmlData.value = ''
+  await attendanceServices.getMonthlyAttendance(payload).then((result) => {
+    const datas = result.data.data.rows
+    for (const [index, item] of datas.entries()) {
+      const dayOff: number = result.data.data.dayOff
+      const years: number = result.data.data.year
+      const weekdays: number = getTotalDaysByMonth(years, (month.value?.value as number)) - dayOff
+      const hadir = item.attendances ? parseInt(item.attendances.hadir) : 0
+      const sakit = item.attendances ? parseInt(item.attendances.sakit) : 0
+      const izin = item.attendances ? parseInt(item.attendances.izin) : 0
+      const alpa = item.attendances ? weekdays - (hadir + sakit + izin) : 0 + weekdays
+      const entry = `<tr>
+      <td class="center">${index + 1}</td>
+      <td>${item.fullname}</td>
+      <td class="center">${grade.value?.label}</td>
+      <td class="center">${hadir}</td>
+      <td class="center">${sakit}</td>
+      <td class="center">${izin}</td>
+      <td class="center">${alpa}</td>
+      </tr>`
+      htmlData.value = htmlData.value + entry
+    }
+  }).catch((error) => {
+    handleErrorResponse(error)
+    modal.closeModal()
   })
 }
 
